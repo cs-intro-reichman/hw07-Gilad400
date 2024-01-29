d0e7cc901021b39ddae31c8f7efd9d094a2ee852
Gilad400 <gilad.shmuel@post.runi.ac.il>
2024-01-29 22:36:23 +0200

    Try 1

--- SpellChecker.java
+++ SpellChecker.java
@@ -11,25 +11,55 @@ public class SpellChecker {
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
+		String tailStr = "";
+		if (str.length() == 1) {
+			return tailStr;
+		} else{
+			tailStr = str.substring(1);
+			return tailStr;
+		}
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+		if (word2.length() == 0) {
+			return word1.length();
+		}
+		if (word1.length() == 0) {
+			return word2.length();
+		}
+		if (word1.charAt(0) == word2.charAt(0)) {
+			return levenshtein(tail(word1), tail(word2));
+		}
+		return 1 + (Math.min(levenshtein(tail(word1), word2), Math.min(levenshtein(word1, tail(word2)), levenshtein(tail(word1), tail(word2)))));
 	}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
-
-		// Your code here
+		int index = 0;
+		while (!in.isEmpty()) {
+			dictionary[index++] = in.readLine(); 
+		}
 
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+		String mostSimilar = "";
+		int minDistance = word.length();
+		for(int i = 0; i < dictionary.length; i++){
+			int tempDistance = levenshtein(word, dictionary[i]);
+			if (tempDistance < minDistance) {
+				minDistance = tempDistance;
+				mostSimilar = dictionary[i];
+			}
+		}
+		if (minDistance > threshold) {
+			return word;
+		}else{
+			return mostSimilar;
+		}
 	}
 
 }
